046fe2984f441e0db16e3832f2eb9ccbffcccfb7
Lorenzo Bufardeci <lobufard@e3r4p3.42roma.it>
2023-03-27 15:54:11 +0200

    add

---
+++ libft/functions/additional/ft_substr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobufard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 14:09:09 by lobufard          #+#    #+#             */
+/*   Updated: 2023/03/27 14:33:41 by lobufard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sbstr;
+
+	i = 0;
+	sbstr = (char *) malloc(sizeof(char) * len + 1);
+	if (sbstr == NULL)
+		return (NULL);
+	while (s[i] && i < len)
+	{
+		sbstr[i] = s[start + i];
+		i ++;
+	}
+	sbstr[i] = '\0';
+	return (sbstr);
+}
