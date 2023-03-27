3edad3b02e0087a2cf15de4d2a51570dcd6c786c
Lorenzo Bufardeci <lobufard@e3r4p3.42roma.it>
2023-03-27 14:08:08 +0200

    calloc

---
+++ libft/functions/libc/ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobufard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 13:55:38 by lobufard          #+#    #+#             */
+/*   Updated: 2023/03/27 13:56:17 by lobufard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
