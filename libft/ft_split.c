d2c68eb4b0ffd15870e1749ae9d6c6f6a9af5607
Lorenzo Bufardeci <lobufard@e3r8p5.42roma.it>
2023-04-04 17:26:58 +0200

    final split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lobufard <lobufard@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 14:10:03 by lobufard          #+#    #+#             */
-/*   Updated: 2023/04/04 16:40:25 by lobufard         ###   ########.fr       */
+/*   Updated: 2023/04/04 17:22:51 by lobufard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,27 @@ static size_t	nbr_of_substr(char const *s, char c)
 	return (nbr);
 }
 
+int	ft_free(char **array, int i)
+{
+	if (array[i] == NULL)
+	{
+		while (i)
+			free(array[i--]);
+		free(array);
+		return (1);
+	}
+	return (0);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	char	**array;
 	size_t	i;
 	size_t	k;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	array = (char **) malloc(sizeof(char *) * (nbr_of_substr(s, c) + 1));
-	if (array == NULL)
+	if (s == NULL || array == NULL)
 		return (NULL);
 	while (*s)
 	{
@@ -53,6 +63,8 @@ char	**ft_split(const char *s, char c)
 			while (*s && *s != c && ++k)
 				++s;
 			array[i++] = ft_substr(s - k, 0, k);
+			if (ft_free(array, i - 1))
+				return (NULL);
 		}
 		else
 			++s;
