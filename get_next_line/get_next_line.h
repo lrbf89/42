70bb7e21f8e6d1751e23346e5f12e470784a6d4d
lorenzo <lorenzo.bufardeci@gmail.com>
2023-04-19 00:59:13 +0200

    UPDATE FINAL

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lobufard <lobufard@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 02:17:51 by lobufard          #+#    #+#             */
-/*   Updated: 2023/04/14 02:27:35 by lobufard         ###   ########.fr       */
+/*   Updated: 2023/04/19 00:59:02 by lobufard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ char	*get_next_line(int fd);
 //utils
 size_t	chrpos(const char *s, int c);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
 
